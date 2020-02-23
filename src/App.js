77adca307bce1f8e05dbf8eab6600786965289cc
amitrai <aforamitrai@gmail.com>
2020-02-23 12:46:18 +0530

    expense tracker final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Blance from "./components/Blance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import Transaction from "./components/Transaction";
+import AddTransecction from "./components/AddTransecction";
+import { GlobelProvider } from "./context/GlobelState";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobelProvider>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Blance />
+          <IncomeExpenses />
+          <Transaction />
+          <AddTransecction />
+        </div>
+      </div>
+    </GlobelProvider>
   );
 }
 
